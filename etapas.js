8cfbde12b583d6a41606f1cedbd430a6a403dd2f
gabriela.ttavares <gabriela.ttavares@gmail.com>
2022-02-18 14:27:22 -0300

    :clipboard: Layout urna eletrônica

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '15311',
+                nome: 'Fulado de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: 'burns.JPG', legenda: 'Vereador', small:false}
+                ]
+            },
+            {
+                numero: '38111',
+                nome: 'Fulado de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: 'edna.JPG', legenda: 'Vereador', }
+                ]
+            },
+         ]
+        },
+            {
+                titulo: 'PREFEITO',
+                numeros: 2,
+                candidatos:[
+                    {
+                        numero:'99',
+                        nome: 'Ciclano',
+                        partido: 'ABD',
+                        vice: 'Cic',
+                        fotos:[
+                            {url:'milhouse.JPG', legenda: 'Prefeito'},
+                            {url: 'palhaço.JPG', legenda: 'Vice-Prefeito', small: true}
+                        ]
+                    },
+                    {
+                        numero:'96',
+                        nome: 'Ciclaninho',
+                        partido: 'ABf',
+                        vice: 'Cici',
+                        fotos:[
+                            {url:'edna.JPG', legenda: 'Prefeito'},
+                            {url: 'burns.JPG', legenda: 'Vice-Prefeito', small: true}
+                        ]
+                    },
+                ]
+            }
+        ];
+        
+    
